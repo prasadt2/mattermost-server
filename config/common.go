aaec96fa4f60509c4d9b66874c59da45825331e0
JoramWilander <jwawilander@gmail.com>
2019-07-31 13:59:40 -0400

    Respect env var overrides when setting config

--- config/common.go
+++ config/common.go
@@ -5,6 +5,7 @@ package config
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"sync"
 
@@ -42,7 +43,7 @@ func (cs *commonStore) GetEnvironmentOverrides() map[string]interface{} {
 // using the persist function argument.
 //
 // This function assumes no lock has been acquired, as it acquires a write lock itself.
-func (cs *commonStore) set(newCfg *model.Config, validate func(*model.Config) error, persist func(*model.Config) error) (*model.Config, error) {
+func (cs *commonStore) set(newCfg *model.Config, allowEnvironmentOverrides bool, validate func(*model.Config) error, persist func(*model.Config) error) (*model.Config, error) {
 	cs.configLock.Lock()
 	var unlockOnce sync.Once
 	defer unlockOnce.Do(cs.configLock.Unlock)
@@ -56,7 +57,17 @@ func (cs *commonStore) set(newCfg *model.Config, validate func(*model.Config) er
 	// 	return nil, errors.New("old configuration modified instead of cloning")
 	// }
 
-	newCfg = newCfg.Clone()
+	// To both clone and re-apply the environment variable overrides we marshal and then
+	// unmarshal the config again.
+	jsonConfig, err := json.Marshal(newCfg)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to marshal config")
+	}
+	newCfg, _, err = unmarshalConfig(bytes.NewReader(jsonConfig), allowEnvironmentOverrides)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to unmarshal config with env overrides")
+	}
+
 	newCfg.SetDefaults()
 
 	// Sometimes the config is received with "fake" data in sensitive fields. Apply the real
